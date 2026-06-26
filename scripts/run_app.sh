0ef72f6f176e9ed2fe5ba0262786c71060937827
shiromin639 <tranphuclongk100@gmail.com>
2026-06-26 20:11:17 +0700

    basic pipeline: dispatcher receive packets from rx queue -> enqueue to worker's ring -> worker sent out packets

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo ./build/flowcore -l 1-5 \
-    --vdev 'net_pcap0,rx_pcap=./pcap/input_traffic.pcap,tx_pcap=./pcap/output_traffic.pcap' \
-    --vdev 'net_pcap1,rx_pcap=./pcap/input_traffic.pcap,tx_pcap=./pcap/output_traffic.pcap'
+sudo ./build/flowcore -l 1-2 \
+    --vdev 'net_pcap0,rx_pcap=./pcap/input_traffic.pcap' \
+    --vdev 'net_pcap1,tx_pcap=./pcap/output_traffic.pcap'
 
--- src/main.c
+++ src/main.c
@@ -1,71 +1,90 @@
 #include <rte_branch_prediction.h>
+#include <rte_common.h>
+#include <rte_launch.h>
+#include <rte_lcore.h>
+#include <rte_log.h>
+#include <rte_mempool.h>
+#include <rte_ring.h>
+#include <rte_ring_core.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <rte_eal.h>
+#include <rte_debug.h>
 #include <rte_ethdev.h>
-#include <rte_lcore.h>
-#include <rte_mbuf.h>
-#define RX_RING_SIZE 1024
-#define TX_RING_SIZE 1024
+#include <string.h>
 
-#define NUM_MBUFS 8191
-#define MBUF_CACHE_SIZE 250
-#define BURST_SIZE 32
+
+
+#define RX_DESC_PER_QUEUE 1024
+#define TX_DESC_PER_QUEUE 1024
+
+#define MAX_PKTS_BURST 32
+#define NUM_MBUFS ((64*1024)-1)
+#define MBUF_CACHE_SIZE 128
+#define BURST_SIZE 64
+#define SCHED_RX_RING_SZ 8192
+#define SCHED_TX_RING_SZ 65536
+#define BURST_SIZE_TX 32
+
+#define RING_SIZE 16384
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+unsigned int portmask;
+struct output_buffer {
+    unsigned count;
+    struct rte_mbuf *mbufs[BURST_SIZE];
+};
+
+struct worker_thread_args {
+    struct rte_ring *ring_in;
+};
 
 static inline int
 port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 {
-    struct rte_eth_conf port_conf; // hold the config of port
-    const uint16_t rx_rings = 1, tx_rings = 1; // number of rx and tx rings of a port
-    uint16_t nb_rxd = RX_RING_SIZE;
-    uint16_t nb_txd = TX_RING_SIZE;
-    struct rte_eth_dev_info dev_info;
-    struct rte_eth_txconf txconf;
+    struct rte_eth_conf port_conf;
+    const uint16_t rx_rings = 1, tx_rings = 1;
+    uint16_t nb_rxd = RX_DESC_PER_QUEUE;
+    uint16_t nb_txd = TX_DESC_PER_QUEUE;
     int retval;
     uint16_t q;
-    
-    // check if the port is valid
+    struct rte_eth_dev_info dev_info;
+    struct rte_eth_txconf txconf;
+
     if (!rte_eth_dev_is_valid_port(port))
         return -1;
-    // refresh the port_conf
+
     memset(&port_conf, 0, sizeof(struct rte_eth_conf));
-    
-    // get the device info
+
     retval = rte_eth_dev_info_get(port, &dev_info);
     if (retval != 0) {
         printf("Error during getting device (port %u) info: %s\n",
                port, strerror(-retval));
         return retval;
     }
-    
-    // eth dev offload 
+
     if (dev_info.tx_offload_capa & RTE_ETH_TX_OFFLOAD_MBUF_FAST_FREE)
         port_conf.txmode.offloads |= RTE_ETH_TX_OFFLOAD_MBUF_FAST_FREE;
-    
-    // configure the port
+
     retval = rte_eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
     if (retval != 0)
         return retval;
-    
-    // spcecify the number of ring descriptor (mbuf) in the rx, tx queue for the port
+
     retval = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
     if (retval != 0)
         return retval;
-    // allocate and setup n rx queue for the port, for this example, n = 1 
+
     for (q = 0; q < rx_rings; q++) {
         retval = rte_eth_rx_queue_setup(port, q, nb_rxd, 
-                                        rte_eth_dev_socket_id(port), 
-                                        NULL, mbuf_pool);
+                                        rte_eth_dev_socket_id(port), NULL, mbuf_pool);
         if (retval < 0)
             return retval;
     }
-    // [UNKOWN], review later 
+
     txconf = dev_info.default_txconf;
     txconf.offloads = port_conf.txmode.offloads;
 
-    // allocate tx queue
-    // a note for this is that tx queue dont need mempool parameter, but need config
     for (q = 0; q < tx_rings; q++) {
         retval = rte_eth_tx_queue_setup(port, q, nb_txd, 
                                         rte_eth_dev_socket_id(port), &txconf);
@@ -73,107 +92,129 @@ port_init(uint16_t port, struct rte_mempool *mbuf_pool)
             return retval;
     }
 
-    // after configure, allocate, then we start the port
     retval = rte_eth_dev_start(port);
     if (retval < 0)
         return retval;
-    
-    // display some information
+
     struct rte_ether_addr addr;
     retval = rte_eth_macaddr_get(port, &addr);
+
     if (retval != 0) {
-        return retval;          
+        return retval;
     }
+
 	printf("Port %u MAC: %02" PRIx8 " %02" PRIx8 " %02" PRIx8
 			   " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 "\n",
 			port, RTE_ETHER_ADDR_BYTES(&addr));
-
-    // enable promiscuous mode (allow all packets to pass)
+    
     retval = rte_eth_promiscuous_enable(port);
     if (retval != 0)
         return retval;
 
     return 0;
 }
-
-// processing function
-static __rte_noreturn void
-lcore_main(void)
+static inline void 
+pktmbuf_free_bulk(struct rte_mbuf *mbuf_table[], unsigned n)
 {
-    uint16_t port;
-    
-    // check whether the port is belong to this numa node
-    RTE_ETH_FOREACH_DEV(port)
-        if (rte_eth_dev_socket_id(port) >= 0 &&
-            rte_eth_dev_socket_id(port) != (int)rte_socket_id())
-            printf("warning, port %u is on remote numa mode to "
-                   "polling thread.\n\tperformance will "
-                   "not be optimal.\n", port);
-    printf("\nCore %u forwarding packets, [Ctrl+C to quit]\n",
-           rte_lcore_id());
+    unsigned int i;
+    for (i = 0; i < n; i++)
+        rte_pktmbuf_free(mbuf_table[i]);
+}
 
-    // main loop: receive and send packets
+static __rte_always_inline int
+dispatcher_thread(struct rte_ring *ring_out)
+{
+    uint16_t ret = 0;
+    uint16_t nb_rx_pkts;
+    uint16_t port_id;
+    struct rte_mbuf *pkts[MAX_PKTS_BURST];
     for (;;) {
-        // receive step
-        RTE_ETH_FOREACH_DEV(port) {
-            // declare an array to hold all the pointers to the burst of mbuf received in mempool
-            struct rte_mbuf *bufs[BURST_SIZE];
-            const uint16_t nb_rx = rte_eth_rx_burst(port, 0, bufs, BURST_SIZE);
-
-            // branch prediction
-            if (unlikely(nb_rx ==0))
-                continue;
-
-            const uint16_t nb_tx = rte_eth_tx_burst(port ^ 1, 0, bufs, nb_rx);
-
-            // free unsent packtes
-            if (unlikely(nb_tx < nb_rx)) {
-                uint16_t buf;
-                // e.g bufs have 8 entries, nb_tx = 3 => unsent from packet index 3 til 7
-                for (buf = nb_tx; buf < nb_rx; buf++)
-                    rte_pktmbuf_free(bufs[buf]);
+        RTE_ETH_FOREACH_DEV(port_id) {
+            nb_rx_pkts = rte_eth_rx_burst(port_id, 0, pkts, MAX_PKTS_BURST);
+            if (likely(nb_rx_pkts > 0)) {
+                printf("Dispatcher received %u packets\n", nb_rx_pkts);
+                ret = rte_ring_enqueue_burst(ring_out, (void *)pkts, nb_rx_pkts, NULL);
+                printf("Dispatcher enqueue %u packets to worker's ring\n", ret);
+                if (unlikely(ret < nb_rx_pkts)) {
+                    pktmbuf_free_bulk(&pkts[ret], nb_rx_pkts -ret);
+                }
             }
-
-
         }
     }
+    return 0;
+}
+
+static int 
+worker_thread(void *args_ptr)
+{
+    uint16_t burst_size = 0;
+    uint16_t nb_tx;
+    uint16_t tx_port = 1;
+    struct worker_thread_args *args;
+    struct rte_mbuf *burst_buffer[MAX_PKTS_BURST] = { NULL };
+    struct rte_ring *ring_in;
+
+    printf("Worker thread started on lcore %u\n", rte_lcore_id());
+    args = (struct worker_thread_args *) args_ptr;
+    ring_in = args->ring_in;
+
+    for (;;) {
+        burst_size = rte_ring_dequeue_burst(ring_in, (void *)burst_buffer, 
+                                            MAX_PKTS_BURST, NULL);
+        if (unlikely(burst_size == 0))
+            continue;
+        printf("Worker dequeued %u packets from ring\n", burst_size);
+        nb_tx = rte_eth_tx_burst(tx_port, 0, burst_buffer, burst_size);
+        printf("Worker sent out %u packets\n", nb_tx);
+        if (unlikely(nb_tx < burst_size)) {
+            pktmbuf_free_bulk(&burst_buffer[nb_tx], burst_size - nb_tx);
+        }
+    }
+    return 0;
 }
 int
 main(int argc, char *argv[])
 {
-    // always declare variable at the very first of a function
-    
     struct rte_mempool *mbuf_pool;
-    unsigned nb_ports;
+    int ret;
+    uint16_t nb_ports;
     uint16_t portid;
+    unsigned int worker_lcore_id, main_lcore_id;
+    struct worker_thread_args worker_args = { NULL };
+    struct rte_ring *dispatcher_to_workers;
+    ret = rte_eal_init(argc, argv);
+    if (ret < 0) 
+        rte_exit(EXIT_FAILURE, "Inivalid EAL arguments\n");
 
-    // 1, init the environment
-    int rte = rte_eal_init(argc, argv);
-    if (rte < 0)
-        rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
+    argc -= ret;
+    argv += ret;
 
-    // check for availability and validity
     nb_ports = rte_eth_dev_count_avail();
-    if (nb_ports < 2 || (nb_ports & 1))
-        rte_exit(EXIT_FAILURE, "Error: number of ports must be even\n");
-    
-    // create the mempool 
-    mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports, 
-                                        MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
-    if (mbuf_pool == NULL)
-        rte_exit(EXIT_FAILURE, "Cannot create mbuf pool\n");
-    
-    // init all the port (inform it to dma into mempool)
-    RTE_ETH_FOREACH_DEV(portid)
+    if (nb_ports == 0) {
+        rte_exit(EXIT_FAILURE, "No ethernet ports -bye\n");
+    }
+	mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports,
+		MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
+
+    RTE_ETH_FOREACH_DEV(portid) {
         if (port_init(portid, mbuf_pool) != 0)
             rte_exit(EXIT_FAILURE, "Cannot init port %"PRIu16 "\n", portid);
+    }
 
-    // utility, just check if we are wasting resources 
-    if (rte_lcore_count() > 1)
-        printf("\nWARNING: Too many lcores enabled. Only 1 use.\n");
+    dispatcher_to_workers = rte_ring_create("dispatcher_to_worker", RING_SIZE,
+                                            rte_socket_id(), RING_F_SC_DEQ);
 
-    // the processing step: receive and send packet via ports
-    lcore_main();
+    main_lcore_id = rte_get_main_lcore();
+    worker_args.ring_in = dispatcher_to_workers; 
+    
+    worker_lcore_id = rte_get_next_lcore(main_lcore_id, 1, 0); 
+    if (worker_lcore_id == RTE_MAX_LCORE) {
+        rte_exit(EXIT_FAILURE, "Not enough core available to run worker!\n");
+    }
+
+    rte_eal_remote_launch(worker_thread, &worker_args, worker_lcore_id);
+    dispatcher_thread(dispatcher_to_workers);
+    rte_eal_mp_wait_lcore();
 
     rte_eal_cleanup();
     return 0;
