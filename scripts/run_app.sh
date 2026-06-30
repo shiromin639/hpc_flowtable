f03ba65f3c6e8b5fe0e80e2be2995c5c59ee7c78
shiromin639 <tranphuclongk100@gmail.com>
2026-06-30 23:35:15 +0700

    basic look up flowtable based on 5-tuple value without statistics

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo ./build/flowcore -l 1-2 \
+sudo ./build/flowcore -l 0-4 \
     --vdev 'net_pcap0,rx_pcap=./pcap/input_traffic.pcap' \
-    --vdev 'net_pcap1,tx_pcap=./pcap/output_traffic.pcap'
-
+    --vdev 'net_pcap1,tx_pcap=./pcap/out_worker0.pcap,tx_pcap=./pcap/out_worker1.pcap,tx_pcap=./pcap/out_worker2.pcap,tx_pcap=./pcap/out_worker3.pcap'
--- src/main.c
+++ src/main.c
@@ -1,221 +1,353 @@
-#include <rte_branch_prediction.h>
+#include <netinet/in.h>
+#include <rte_jhash.h>
+#include <rte_byteorder.h>
 #include <rte_common.h>
+#include <rte_debug.h>
+#include <rte_eal.h>
+#include <rte_ethdev.h>
+#include <rte_ether.h>
 #include <rte_launch.h>
 #include <rte_lcore.h>
-#include <rte_log.h>
-#include <rte_mempool.h>
+#include <rte_mbuf.h>
+#include <rte_mbuf_core.h>
+#include <rte_hash.h>
 #include <rte_ring.h>
 #include <rte_ring_core.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <rte_eal.h>
-#include <rte_debug.h>
-#include <rte_ethdev.h>
-#include <string.h>
-
-
-
+#include <signal.h>
+#define NUM_MBUFS 8191
+#define NUM_WORKERS 4
+#define MBUF_CACHE_SIZE 250
 #define RX_DESC_PER_QUEUE 1024
 #define TX_DESC_PER_QUEUE 1024
+#define RING_SIZE 4096
+#define HASH_ENTRIES (1024*1024)
+#define BURST_SIZE 32
 
-#define MAX_PKTS_BURST 32
-#define NUM_MBUFS ((64*1024)-1)
-#define MBUF_CACHE_SIZE 128
-#define BURST_SIZE 64
-#define SCHED_RX_RING_SZ 8192
-#define SCHED_TX_RING_SZ 65536
-#define BURST_SIZE_TX 32
+#define PORT_IN 0 
+#define PORT_OUT 1
+static struct rte_mempool *mbuf_pool;
+static struct rte_hash *flow_table;
+static struct rte_ring *worker_rings[NUM_WORKERS];
+volatile uint8_t force_quit;
 
-#define RING_SIZE 16384
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-unsigned int portmask;
-struct output_buffer {
-    unsigned count;
-    struct rte_mbuf *mbufs[BURST_SIZE];
+
+struct ipv4_5tuple_key {
+    uint32_t ip_src;
+    uint32_t ip_dst;
+    uint16_t port_src;
+    uint16_t port_dst;
+    uint8_t  proto;
+} __attribute__((__packed__));
+
+typedef struct {
+    uint32_t src_ip;
+    uint32_t dst_ip;
+    uint16_t src_port;
+    uint16_t dst_port;
+
+    uint8_t worker_id;
+    uint64_t create_time;
+    uint64_t last_seen;
+    uint8_t protocol;
+} flow_entry_t;
+
+static flow_entry_t flow_entries[HASH_ENTRIES];
+struct worker_args {
+    uint32_t worker_id;
+};
+static const struct rte_eth_conf port_conf_default = {
+    .rxmode = {
+        .mq_mode = RTE_ETH_MQ_RX_NONE,
+    },
+    .txmode = {
+        .mq_mode = RTE_ETH_MQ_TX_NONE,
+    }
 };
 
-struct worker_thread_args {
-    struct rte_ring *ring_in;
-};
-
-static inline int
-port_init(uint16_t port, struct rte_mempool *mbuf_pool)
+static inline int 
+ports_init(void)
 {
-    struct rte_eth_conf port_conf;
-    const uint16_t rx_rings = 1, tx_rings = 1;
+    int ret;
+    uint16_t rx_rings = 1, tx_rings = 1;
+    struct rte_eth_conf port_conf = port_conf_default;
     uint16_t nb_rxd = RX_DESC_PER_QUEUE;
     uint16_t nb_txd = TX_DESC_PER_QUEUE;
-    int retval;
     uint16_t q;
-    struct rte_eth_dev_info dev_info;
-    struct rte_eth_txconf txconf;
-
-    if (!rte_eth_dev_is_valid_port(port))
-        return -1;
-
-    memset(&port_conf, 0, sizeof(struct rte_eth_conf));
-
-    retval = rte_eth_dev_info_get(port, &dev_info);
-    if (retval != 0) {
-        printf("Error during getting device (port %u) info: %s\n",
-               port, strerror(-retval));
-        return retval;
-    }
-
-    if (dev_info.tx_offload_capa & RTE_ETH_TX_OFFLOAD_MBUF_FAST_FREE)
-        port_conf.txmode.offloads |= RTE_ETH_TX_OFFLOAD_MBUF_FAST_FREE;
-
-    retval = rte_eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
-    if (retval != 0)
-        return retval;
-
-    retval = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
-    if (retval != 0)
-        return retval;
-
-    for (q = 0; q < rx_rings; q++) {
-        retval = rte_eth_rx_queue_setup(port, q, nb_rxd, 
-                                        rte_eth_dev_socket_id(port), NULL, mbuf_pool);
-        if (retval < 0)
-            return retval;
-    }
-
-    txconf = dev_info.default_txconf;
-    txconf.offloads = port_conf.txmode.offloads;
-
-    for (q = 0; q < tx_rings; q++) {
-        retval = rte_eth_tx_queue_setup(port, q, nb_txd, 
-                                        rte_eth_dev_socket_id(port), &txconf);
-        if (retval < 0)
-            return retval;
-    }
-
-    retval = rte_eth_dev_start(port);
-    if (retval < 0)
-        return retval;
-
-    struct rte_ether_addr addr;
-    retval = rte_eth_macaddr_get(port, &addr);
-
-    if (retval != 0) {
-        return retval;
-    }
-
-	printf("Port %u MAC: %02" PRIx8 " %02" PRIx8 " %02" PRIx8
-			   " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 "\n",
-			port, RTE_ETHER_ADDR_BYTES(&addr));
+    int socket_id_in = rte_eth_dev_socket_id(PORT_IN);
+    int socket_id_out = rte_eth_dev_socket_id(PORT_OUT);
     
-    retval = rte_eth_promiscuous_enable(port);
-    if (retval != 0)
-        return retval;
+    // set up port in
+    ret = rte_eth_dev_configure(PORT_IN, rx_rings, tx_rings, &port_conf); 
+    if (ret != 0)
+        return ret;
+    for (q = 0; q < rx_rings; q++) {
+        ret = rte_eth_rx_queue_setup(PORT_IN, q, nb_rxd, 
+                                     socket_id_in, NULL, mbuf_pool);
+        if (ret < 0) {
+            printf("Cannot set up rx queue for port in\n");
+            return ret;
+        }
+    }
+    rte_eth_tx_queue_setup(PORT_IN, 0, nb_txd, socket_id_in, NULL);
 
+    // set up port out
+    tx_rings = 4;
+    ret = rte_eth_dev_configure(PORT_OUT, rx_rings, tx_rings, &port_conf);
+    if (ret != 0) {
+        printf("Cannot configure port out\n");
+        return ret;
+    }
+    rte_eth_rx_queue_setup(PORT_OUT, 0, nb_rxd, socket_id_out, 
+                           NULL, mbuf_pool);
+    for (q = 0; q < tx_rings; q++) {
+        ret = rte_eth_tx_queue_setup(PORT_OUT, q, nb_txd, socket_id_out, NULL);  
+        if (ret != 0) {
+            printf("Cannot set up tx queue %d for port out\n", q);
+        }
+    }
+
+    // start the ports
+    ret = rte_eth_dev_start(PORT_IN);
+    if (ret != 0) {
+        printf("Cannot start port in\n");
+        return ret;
+    }
+    ret = rte_eth_dev_start(PORT_OUT);
+    if (ret != 0) {
+        printf("Cannot start port out\n");
+        return ret;
+    }
+    rte_eth_promiscuous_enable(PORT_IN);
+    rte_eth_promiscuous_enable(PORT_OUT);
     return 0;
-}
-static inline void 
-pktmbuf_free_bulk(struct rte_mbuf *mbuf_table[], unsigned n)
-{
-    unsigned int i;
-    for (i = 0; i < n; i++)
-        rte_pktmbuf_free(mbuf_table[i]);
+        
 }
 
-static __rte_always_inline int
-dispatcher_thread(struct rte_ring *ring_out)
+
+static int
+dispatcher_thread(__rte_unused void *arg)
 {
-    uint16_t ret = 0;
-    uint16_t nb_rx_pkts;
-    uint16_t port_id;
-    struct rte_mbuf *pkts[MAX_PKTS_BURST];
-    for (;;) {
-        RTE_ETH_FOREACH_DEV(port_id) {
-            nb_rx_pkts = rte_eth_rx_burst(port_id, 0, pkts, MAX_PKTS_BURST);
-            if (likely(nb_rx_pkts > 0)) {
-                printf("Dispatcher received %u packets\n", nb_rx_pkts);
-                ret = rte_ring_enqueue_burst(ring_out, (void *)pkts, nb_rx_pkts, NULL);
-                printf("Dispatcher enqueue %u packets to worker's ring\n", ret);
-                if (unlikely(ret < nb_rx_pkts)) {
-                    pktmbuf_free_bulk(&pkts[ret], nb_rx_pkts -ret);
+    struct rte_mbuf *pkts_burst[BURST_SIZE];
+    struct rte_mbuf *worker_buffers[NUM_WORKERS][BURST_SIZE];
+    uint16_t worker_counts[NUM_WORKERS];
+    int i, w;
+
+    printf("Dispatcher is running on lcore %u\n", rte_lcore_id());
+
+    while (!force_quit) {
+        memset(worker_counts, 0, sizeof(worker_counts));
+        
+        const uint16_t nb_rx = rte_eth_rx_burst(PORT_IN, 0, pkts_burst, BURST_SIZE);
+        if (unlikely(nb_rx == 0))
+            continue;
+        
+        /* Lấy thời gian hiện tại một lần cho cả batch (tiết kiệm cycle) */
+        uint64_t current_tsc = rte_rdtsc();
+
+        for (i = 0; i < nb_rx; i++) {
+            struct rte_mbuf *m = pkts_burst[i];
+            struct rte_ether_hdr *eth_hdr = rte_pktmbuf_mtod(m, struct rte_ether_hdr *);
+
+            if (eth_hdr->ether_type != rte_cpu_to_be_16(RTE_ETHER_TYPE_IPV4)) {
+                rte_pktmbuf_free(m);
+                continue;
+            }
+
+            struct rte_ipv4_hdr *ipv4_hdr = (struct rte_ipv4_hdr *)(eth_hdr + 1);
+            
+            if (ipv4_hdr->next_proto_id != IPPROTO_TCP && ipv4_hdr->next_proto_id != IPPROTO_UDP) {
+                rte_pktmbuf_free(m);
+                continue;
+            }
+
+            /* Tạo Key */
+            struct ipv4_5tuple_key key;
+            key.ip_src = ipv4_hdr->src_addr;
+            key.ip_dst = ipv4_hdr->dst_addr;
+            key.proto  = ipv4_hdr->next_proto_id;
+
+            uint16_t *ports = (uint16_t *)((unsigned char *)ipv4_hdr + sizeof(struct rte_ipv4_hdr));
+            key.port_src = ports[0];
+            key.port_dst = ports[1];
+
+            /* ====================================================
+             * CORE LOGIC: QUẢN LÝ FLOW TABLE & ROUTING
+             * ==================================================== */
+            uint32_t target_worker_id;
+            
+            /* Tìm flow trong bảng băm */
+            int flow_index = rte_hash_lookup(flow_table, &key);
+            
+            if (flow_index < 0) {
+                /* KHÔNG TÌM THẤY -> ĐÂY LÀ FLOW MỚI */
+                flow_index = rte_hash_add_key(flow_table, &key);
+                
+                if (likely(flow_index >= 0)) {
+                    /* Hash table trả về vị trí index, ta ghi đè vào mảng flow_entries */
+                    flow_entries[flow_index].src_ip = key.ip_src;
+                    flow_entries[flow_index].dst_ip = key.ip_dst;
+                    flow_entries[flow_index].src_port = key.port_src;
+                    flow_entries[flow_index].dst_port = key.port_dst;
+                    flow_entries[flow_index].protocol = key.proto;
+                    
+                    /* Phân công Worker (Round-robin hoặc Hash). Ở đây dùng Hash modulo */
+                    hash_sig_t hash_val = rte_hash_hash(flow_table, &key);
+                    target_worker_id = hash_val % NUM_WORKERS;
+                    
+                    flow_entries[flow_index].worker_id = target_worker_id;
+                    flow_entries[flow_index].create_time = current_tsc;
+                    flow_entries[flow_index].last_seen = current_tsc;
+                } else {
+                    /* LỖI: Bảng băm đã đầy (Hash table full) -> Drop packet */
+                    rte_pktmbuf_free(m);
+                    continue; 
+                }
+            } else {
+                /* ĐÃ TÌM THẤY -> FLOW ĐANG TỒN TẠI */
+                /* flow_index lúc này là index của flow cũ */
+                target_worker_id = flow_entries[flow_index].worker_id;
+                
+                /* Cập nhật timestamp để sau này làm cơ sở xóa flow (timeout) */
+                flow_entries[flow_index].last_seen = current_tsc;
+            }
+
+            /* ==================================================== */
+
+            /* Đẩy gói tin vào buffer của worker tương ứng */
+            worker_buffers[target_worker_id][worker_counts[target_worker_id]++] = m;
+        }
+
+        /* Đẩy từ buffer tạm vào Ring (Tương tự như cũ) */
+        for (w = 0; w < NUM_WORKERS; w++) {
+            if (worker_counts[w] > 0) {
+                uint16_t sent = rte_ring_enqueue_burst(worker_rings[w], 
+                                                      (void **)worker_buffers[w], 
+                                                      worker_counts[w], NULL);
+                if (unlikely(sent < worker_counts[w])) {
+                    for (i = sent; i < worker_counts[w]; i++) {
+                        rte_pktmbuf_free(worker_buffers[w][i]);
+                    }
                 }
             }
         }
     }
     return 0;
 }
-
 static int 
-worker_thread(void *args_ptr)
+worker_thread(void *arg)
 {
-    uint16_t burst_size = 0;
-    uint16_t nb_tx;
-    uint16_t tx_port = 1;
-    struct worker_thread_args *args;
-    struct rte_mbuf *burst_buffer[MAX_PKTS_BURST] = { NULL };
-    struct rte_ring *ring_in;
+    struct worker_args *wargs = (struct worker_args *)arg;
+    uint32_t worker_id = wargs->worker_id;
+    struct rte_mbuf *pkts[BURST_SIZE];
+    uint16_t nb_rx, nb_tx;
+    uint16_t i;
 
-    printf("Worker thread started on lcore %u\n", rte_lcore_id());
-    args = (struct worker_thread_args *) args_ptr;
-    ring_in = args->ring_in;
+    printf("Worker %u is running on lcore %u\n", worker_id, rte_lcore_id());
 
-    for (;;) {
-        burst_size = rte_ring_dequeue_burst(ring_in, (void *)burst_buffer, 
-                                            MAX_PKTS_BURST, NULL);
-        if (unlikely(burst_size == 0))
+    while (!force_quit) {
+        nb_rx = rte_ring_dequeue_burst(worker_rings[worker_id], (void **)pkts,
+                                       BURST_SIZE, NULL);
+        if (unlikely(nb_rx == 0)) 
             continue;
-        printf("Worker dequeued %u packets from ring\n", burst_size);
-        nb_tx = rte_eth_tx_burst(tx_port, 0, burst_buffer, burst_size);
-        printf("Worker sent out %u packets\n", nb_tx);
-        if (unlikely(nb_tx < burst_size)) {
-            pktmbuf_free_bulk(&burst_buffer[nb_tx], burst_size - nb_tx);
+
+        nb_tx = rte_eth_tx_burst(PORT_OUT, worker_id, pkts, nb_rx);
+
+        if (unlikely(nb_tx < nb_rx)) {
+            for (i = nb_tx; i < nb_rx; i++) {
+                rte_pktmbuf_free(pkts[i]);
+            }
         }
+
     }
     return 0;
 }
-int
+
+static void
+int_handler(int sig_num)
+{
+    printf("Exiting on signal %d\n", sig_num);
+    force_quit = 1;
+}
+int 
 main(int argc, char *argv[])
 {
-    struct rte_mempool *mbuf_pool;
-    int ret;
-    uint16_t nb_ports;
-    uint16_t portid;
-    unsigned int worker_lcore_id, main_lcore_id;
-    struct worker_thread_args worker_args = { NULL };
-    struct rte_ring *dispatcher_to_workers;
+    int ret, i;
+    unsigned nb_ports; 
+    unsigned lcore_id; 
+
+    signal(SIGINT, int_handler);
     ret = rte_eal_init(argc, argv);
-    if (ret < 0) 
-        rte_exit(EXIT_FAILURE, "Inivalid EAL arguments\n");
-
-    argc -= ret;
-    argv += ret;
-
-    nb_ports = rte_eth_dev_count_avail();
-    if (nb_ports == 0) {
-        rte_exit(EXIT_FAILURE, "No ethernet ports -bye\n");
+    if (ret < 0) {
+        rte_exit(EXIT_FAILURE, "Cannot init eal\n");
     }
-	mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports,
-		MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
-
-    RTE_ETH_FOREACH_DEV(portid) {
-        if (port_init(portid, mbuf_pool) != 0)
-            rte_exit(EXIT_FAILURE, "Cannot init port %"PRIu16 "\n", portid);
-    }
-
-    dispatcher_to_workers = rte_ring_create("dispatcher_to_worker", RING_SIZE,
-                                            rte_socket_id(), RING_F_SC_DEQ);
-
-    main_lcore_id = rte_get_main_lcore();
-    worker_args.ring_in = dispatcher_to_workers; 
     
-    worker_lcore_id = rte_get_next_lcore(main_lcore_id, 1, 0); 
-    if (worker_lcore_id == RTE_MAX_LCORE) {
-        rte_exit(EXIT_FAILURE, "Not enough core available to run worker!\n");
+    nb_ports = rte_eth_dev_count_avail();
+    if (nb_ports < 1) {
+        rte_exit(EXIT_FAILURE, "Not enough ports\n");
+    }
+    
+    if (rte_lcore_count() < NUM_WORKERS + 1) {
+        rte_exit(EXIT_FAILURE, "Need at least %d cores\n", NUM_WORKERS + 1);
     }
 
-    rte_eal_remote_launch(worker_thread, &worker_args, worker_lcore_id);
-    dispatcher_thread(dispatcher_to_workers);
-    rte_eal_mp_wait_lcore();
+    mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports, 
+                                        MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, 
+                                        rte_socket_id());
+    if (mbuf_pool == NULL) {
+        rte_exit(EXIT_FAILURE, "Cannot allocate mbuf pool\n");
+    }
 
-    rte_eal_cleanup();
+    ret = ports_init();
+    if (ret != 0) {
+        rte_exit(EXIT_FAILURE, "Cannit init ports\n");
+    }
+
+    struct rte_hash_parameters hash_params = {
+        .name = "flow_table",
+        .entries = HASH_ENTRIES,
+        .key_len = sizeof(struct ipv4_5tuple_key),
+        .hash_func = rte_jhash,
+        .hash_func_init_val = 0,
+        .socket_id = rte_socket_id(),
+    };
+    
+    flow_table = rte_hash_create(&hash_params);
+    if (flow_table == NULL) {
+        rte_exit(EXIT_FAILURE, "Cannot create hash_table\n");
+    }
+
+    for (i = 0; i < NUM_WORKERS; i++) {
+        char name[32];
+        snprintf(name, sizeof(name), "worker_ring_%d", i);
+
+        worker_rings[i] = rte_ring_create(name, RING_SIZE, rte_socket_id(), 
+                                          RING_F_SP_ENQ | RING_F_SC_DEQ);
+        if (worker_rings[i] == NULL) {
+            rte_exit(EXIT_FAILURE, "Cannot create ring %d\n", i);
+        }
+    }
+
+    force_quit = 0;
+    struct worker_args wargs[NUM_WORKERS];
+    uint32_t w_id = 0;
+    
+    RTE_LCORE_FOREACH_WORKER(lcore_id) {
+        if (w_id >= NUM_WORKERS) break;
+        wargs[w_id].worker_id = w_id;
+        rte_eal_remote_launch(worker_thread, &wargs[w_id], lcore_id);
+        w_id++;
+    }
+
+    dispatcher_thread(NULL);
+
+    for (i = 0; i < NUM_WORKERS; i++) {
+        rte_ring_free(worker_rings[i]);
+    }
+
+    rte_hash_free(flow_table);
+
     return 0;
 }
